203fb96742bbd519654413e52ef38ee6b05f1071
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-15 20:25:37 +0300

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Text;
+
+namespace Practice_2023;
+
+class Login
+{
+    static void Main(string[] args)
+    {
+        string userName = Console.ReadLine();
+        char[] passArr = userName.Reverse().ToArray();
+        string pass = String.Join("", passArr);
+        int loigInAttemnpts = 4;
+
+        while (true)
+        {
+            string attempt = Console.ReadLine();
+            loigInAttemnpts--;
+
+            if (attempt == pass)
+            {
+                Console.WriteLine($"User {userName} logged in.");
+                return;
+            }
+
+            if (loigInAttemnpts == 0)
+            {
+                Console.WriteLine($"User {userName} blocked!");
+                return;
+            }
+
+            if (attempt != pass)
+            {
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+        }
+    }
+}
+
