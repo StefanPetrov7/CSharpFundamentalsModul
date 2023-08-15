b2ce7215934519cff2da05bce6691f75abb00027
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-15 19:54:26 +0300

    Add files via upload

---
+++ PrintAndSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Text;
+
+namespace Practice_2023;
+
+class PrintAndSum
+{
+    static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        StringBuilder resultText = new StringBuilder();
+        int resultNumber = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            resultText.Append(i.ToString() + " ");
+            resultNumber += i;
+        }
+
+        resultText.AppendLine("\n" + $"Sum: {resultNumber}");
+        Console.WriteLine(resultText);
+    }
+}
+
