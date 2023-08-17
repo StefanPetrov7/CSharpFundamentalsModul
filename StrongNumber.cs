414f44b1f6fe6db29cbafa924c80a460d38edeeb
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-17 22:46:38 +0300

    Add files via upload

---
+++ StrongNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+
+namespace Practice_2023;
+
+class StrongNumber
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CheckIfNumberIsStron(int.Parse(Console.ReadLine())));
+    }
+
+    public static int CalFact(int num)
+    {
+        int sum = 1;
+
+        if (num == 0)
+        {
+            return sum;
+        }
+
+        for (int i = 1; i < num; i++)
+        {
+            sum *= (i + 1);
+        }
+        return sum;
+    }
+
+    public static int SumOfFact(string numAsText)
+    {
+        int[] arr = new int[numAsText.ToString().Length];
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(numAsText[i].ToString());
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += CalFact(arr[i]);
+        }
+        return sum;
+    }
+
+    public static string CheckIfNumberIsStron(int num)
+    {
+        string numAsText = num.ToString();
+
+        if (num == SumOfFact(numAsText))
+        {
+            return "yes";
+        }
+        else
+        {
+            return "no";
+        }
+    }
+}
