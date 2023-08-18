cd6307b364128564eeb1d0fc9272979041eff1dc
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-18 16:59:52 +0300

    Add files via upload

---
+++ TriangleOfNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+
+namespace Practice_2023;
+
+class TriangleOfNum
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= n; i++)
+        {
+            int row = i;
+
+            for (int j = 0; j < row; j++)
+            {
+                Console.Write(row + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
