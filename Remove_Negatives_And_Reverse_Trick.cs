9dab5befd18f8ee1cde5c7b399c0457625641610
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-20 16:31:31 +0300

    Add files via upload

---
+++ Remove_Negatives_And_Reverse_Trick.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice_2023;
+
+class Remove_Negatives_And_Reverse_Trick
+{
+    static void Main(string[] args)
+    {
+        List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        nums = nums.Where(x => x >= 0).ToList();
+        nums.Reverse();
+        Console.WriteLine(String.Join(" ", nums));
+
+    }
+}
