c36fba0c911ea24464ea6c093f5cf04be5818445
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-20 15:51:54 +0300

    Add files via upload

---
+++ Gauss_Trick.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice_2023;
+
+class Gauss_Trick
+{
+    static void Main(string[] args)
+    {
+
+        List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        for (int i = 0; i < nums.Count / 2; i++)
+        {
+            nums[i] += nums[nums.Count - 1 - i];
+        }
+
+        nums.RemoveRange(nums.Count - nums.Count / 2, nums.Count / 2);
+
+        Console.WriteLine(String.Join(" ", nums));
+
+    }
+
+}
