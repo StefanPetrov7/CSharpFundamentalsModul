e393cc9c429c76801575effd5260ad13d2836594
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-26 18:33:40 +0300

    Add files via upload

---
+++ CountRealNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice_2023;
+
+class CountRealNumbers
+{
+    static void Main(string[] args)
+    {
+
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Dictionary<int, int> result = new Dictionary<int, int>();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (result.ContainsKey(numbers[i]))
+            {
+                result[numbers[i]] += 1;
+            }
+            else
+            {
+                result.Add(numbers[i], 1);
+            }
+        }
+
+        foreach (var kvp in result.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+        }
+    }
+}
