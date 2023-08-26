d6175be075e2a96e5e24281c0b9c8a6730cfd006
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-26 23:46:39 +0300

    Add files via upload

---
+++ Largest_Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace Practice_2023;
+
+class Largest_Numbers
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .OrderByDescending(x => x)
+            .ToArray();
+
+        int counter = 0;
+
+        foreach (var n in nums)
+        {
+            if (counter == 3) return;
+            Console.Write(n + " ");
+            counter++;
+        }
+    }
+}
