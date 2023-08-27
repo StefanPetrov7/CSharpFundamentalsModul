67cfa5af063a1376d12f61fc56f3c7cf08095b1b
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-08-27 13:34:08 +0300

    Add files via upload

---
+++ Word_Filter.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace Practice_2023;
+
+class Word_Filter
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => x.Length % 2 == 0)
+            .ToList()
+            .ForEach(word => Console.WriteLine(word));
+    }
+}
