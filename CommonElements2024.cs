7c3ce1da5524dff40aa2152dab1e8243dd8b7633
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2024-08-29 17:56:24 +0300

    Add files via upload

---
+++ CommonElements2024.cs
@@ -0,0 +1,26 @@
+﻿namespace CommonElements2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrOne = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] arrTwo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<string> result = new List<string>();   
+
+            for (int i = 0; i < arrTwo.Length; i++) 
+            {
+                for (int j = 0; j < arrOne.Length; j++)
+                {
+                    if (arrTwo[i] == arrOne[j])
+                    {
+                        result.Add(arrOne[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', result));
+        }
+    }
+}
+
