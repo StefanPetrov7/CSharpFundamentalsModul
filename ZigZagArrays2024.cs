9f05b49be1ea08a11b9df50f29c77173febd27ec
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2024-08-29 23:42:56 +0300

    Add files via upload

---
+++ ZigZagArrays2024.cs
@@ -0,0 +1,34 @@
+﻿namespace ZigZagArrays2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arrOne = new string[n];
+            string[] arrTwo = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (i % 2 == 0)
+                {
+                    arrOne[i] = input[0];
+                    arrTwo[i] = input[1];
+                }
+                else
+                {
+                    arrOne[i] = input[1];
+                    arrTwo[i] = input[0];
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', arrOne));
+            Console.WriteLine(String.Join(' ', arrTwo));
+
+        }
+    }
+}
+
+
