e33f4c734f3b046816fad31a8378d999ca47b042
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2024-08-30 13:32:43 +0300

    Add files via upload

---
+++ ArrayRotation.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int firslEl = arr[0];
+
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+
+                arr[arr.Length-1] = firslEl;
+
+            }
+
+            Console.WriteLine(String.Join(' ', arr));
+        }
+    }
+}
