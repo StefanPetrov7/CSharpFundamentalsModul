3ec2a57f713518df8397ad6d1327c8e7f195593e
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2024-08-30 15:05:02 +0300

    Add files via upload

---
+++ TopIntegers.cs
@@ -0,0 +1,36 @@
+﻿namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int element = arr[i];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (element <= arr[j])
+                    {
+                        break;
+                    }
+
+                    if (j == arr.Length - 1)
+                    {
+                        result.Add(element);
+                    }
+                }
+
+            }
+
+            result.Add(arr[arr.Length - 1]);
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
+
+
